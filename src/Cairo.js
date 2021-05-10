888a275d2187a54dc5029ac38bc75d7957980693
Tristan Cacqueray <tdecacqu@redhat.com>
2021-05-10 15:08:19 +0000

    Add Cairo showText

--- src/Cairo.js
+++ src/Cairo.js
@@ -1,6 +1,12 @@
 "use strict";
 
+exports.moveTo = (cr) => x => y => () => cr.moveTo(x, y)
+
 exports.translate = (cr) => x => y => () => cr.translate(x, y)
+
+exports.setSourceRGB = (cr) => r => g => b => () => cr.setSourceRGB(r, g, b)
+
+exports.showText = (cr) => txt => () => cr.showText(txt)
 
 exports.arc = (cr) => x => y => r => a1 => a2 => () => cr.arc(x, y, r, a1, a2)
 
--- src/Cairo.purs
+++ src/Cairo.purs
@@ -5,7 +5,14 @@ import Effect (Effect)
 
 foreign import data Context :: Type
 
+-- https://www.cairographics.org/manual/cairo-Paths.html#cairo-move-to
+foreign import moveTo :: Context -> Number -> Number -> Effect Unit
+
 foreign import translate :: Context -> Number -> Number -> Effect Unit
+
+foreign import setSourceRGB :: Context -> Number -> Number -> Number -> Effect Unit
+
+foreign import showText :: Context -> String -> Effect Unit
 
 foreign import arc :: Context -> Number -> Number -> Number -> Number -> Number -> Effect Unit
 
