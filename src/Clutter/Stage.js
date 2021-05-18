dc70fa76603981f8d692b9eb83372529fd225e37
Tristan Cacqueray <tdecacqu@redhat.com>
2021-05-18 18:52:56 +0000

    Add Clutter.Stage.get_key_focus

---
+++ src/Clutter/Stage.js
@@ -0,0 +1,3 @@
+"use strict"
+
+exports.unsafe_get_key_focus = stage => () => stage.get_key_focus()
--- src/Clutter/Stage.purs
+++ src/Clutter/Stage.purs
@@ -1,5 +1,6 @@
 module Clutter.Stage where
 
+import Effect (Effect)
 import GObject (class GObject)
 import Clutter.Actor (class Actor)
 
@@ -7,3 +8,8 @@ foreign import data Stage :: Type
 
 instance obj :: GObject Stage
 instance actor :: Actor Stage
+
+foreign import unsafe_get_key_focus :: forall actor. Stage -> Effect actor
+
+get_key_focus :: forall actor. Actor actor => Stage -> Effect actor
+get_key_focus = unsafe_get_key_focus
