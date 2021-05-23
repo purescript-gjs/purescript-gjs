a933a1cc50232e646a78c948345818a2c956a143
Tristan Cacqueray <tdecacqu@redhat.com>
2021-05-23 13:42:33 +0000

    Remove left-over modules for gnome-shell

--- src/ExtensionUtils.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-exports.getCurrentExtension = () => imports.misc.extensionUtils.getCurrentExtension()
-
-exports.getPath = (ext) => name => () => ext.dir.get_child(name).get_path()
--- src/ExtensionUtils.purs
+++
@@ -1,9 +0,0 @@
-module ExtensionUtils where
-
-import Effect (Effect)
-
-foreign import data Extension :: Type
-
-foreign import getCurrentExtension :: Effect Extension
-
-foreign import getPath :: Extension -> String -> Effect String
