5e359557350d6d340eda1a46959f1d4a877d1983
Tristan Cacqueray <tdecacqu@redhat.com>
2024-01-14 16:03:56 -0500

    Add ThemedIcon documentation

--- src/Gio/ThemedIcon.purs
+++ src/Gio/ThemedIcon.purs
@@ -5,5 +5,6 @@ import Gio.Icon (Icon)
 
 foreign import new_ :: String -> Effect Icon
 
+-- | See https://specifications.freedesktop.org/icon-naming-spec/icon-naming-spec-latest.html
 new :: String -> Effect Icon
 new = new_
